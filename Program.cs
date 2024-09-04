bce68b916d99eff2a9caada1b2d5407e25ca87a2
athwtrrrr <athwtr@gmail.com>
2024-09-04 17:56:03 +1000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
-﻿namespace GuessNumber;
+var builder = WebApplication.CreateBuilder(args);
 
-class Program
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    static void Main(string[] args)
-    {
-
-        GuessNumberGame game = new GuessNumberGame();
-
-        // Run the game
-        game.Run();
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
